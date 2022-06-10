33151f0240ba1fe92023d7f784002b1c56ae97a6
KevinDo76 <42784039+KevinDo76@users.noreply.github.com>
2022-06-09 20:11:34 -0700

    command processor update
    this add in the basic code for the command processor and the layout commands source files.

--- src/Coldnix/Commands/test.lua
+++ src/Coldnix/Commands/test.lua
@@ -1,7 +1,9 @@
 local module = {}
-    module.commandName="test"
+    module.name="test"
     module.description="this is a test program, very nice"
-    module.func = function ()
-        print("nice one nerd")
+    module.func = function (rawText)
+        print("this is a test command")
+        print("name: "..module.name)
+        print("description: "..module.description)
     end
 return module
--- src/Coldnix/Kernel/CommandProcessor.lua
+++ src/Coldnix/Kernel/CommandProcessor.lua
@@ -1,13 +1,32 @@
 --all commands code are stored in lua file udner the %commandDir% folder that's going to be check and loaded on start up
 local commandDir="/Coldnix/Commands"
-local keepinmem=config.configList.LOADCOMMANDCODEINTOMEM
+local keepinmem=tonumber(config.configList.LOADCOMMANDCODEINTOMEM)
 local commandDirList=BOOTDRIVEPROXY.list(commandDir) or {}
 local validCommands={}
 for i=1,#commandDirList do
-    
+    local file=loadfile(commandDir.."/"..commandDirList[i])()
+    if file.name and file.description and file.name then
+        if keepinmem==1 then
+            validCommands[file.name]=file
+        else
+            validCommands[file.name]={file.name,file.description,commandDirList[i]}
+        end
+    else
+        print("failed to load "..commandDir.."/"..commandDirList[i])
+    end
 end
 local function response(rawText)
-    
+    local splitText=string.split(rawText," ")
+    local commandName=splitText[1]
+    if validCommands[commandName] then
+        if keepinmem==1 then
+            validCommands[commandName].func(rawText)
+        else
+            local metadata=validCommands[commandName]
+            local file=loadfile(commandDir.."/"..metadata[3])()
+            file.func(rawText)
+        end
+    end
 end
 
 terminal.commandProcessor = response
